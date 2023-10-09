1248bafc08c086b93029af6091a08dc275ff22db
Twenmod <Twengames@gmail.com>
2023-10-09 17:54:54 +0200

    Added labels

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("keras_model.h5", compile=False)
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
@@ -22,8 +22,8 @@ while True:
     # Resize the raw image into (224-height,224-width) pixels
     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+
+
 
     # Make the image a numpy array and reshape it to the models input shape.
     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
@@ -38,8 +38,14 @@ while True:
     confidence_score = prediction[0][index]
 
     # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
+    print("Class:", class_name[0:], end="")
     print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    
+
+    # Show the image in a window
+    #cv2.putText(image, "Hello world")
+    cv2.imshow("Webcam Image", image)
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
